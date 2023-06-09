bbf9f7147d441636f3107a7af1c0b089f350475e
zubayer22 <zubayerkh@gmail.com>
2023-06-09 11:37:08 +0600

    Single class and instructor page route setup complete

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yzqnnly.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,13 +36,29 @@ async function run() {
         const classCollection = client.db("campDb").collection("classes");
         const instructorCollection = client.db("campDb").collection("instructors");
 
+        //classes apis
+
         app.get('/classes', async (req, res) => {
             const result = await classCollection.find().toArray();
             res.send(result);
         })
+        app.get('/classes/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await classCollection.findOne(query);
+            res.send(result);
+        })
 
+        //instructor apis
         app.get('/instructors', async (req, res) => {
             const result = await instructorCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/instructors/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await instructorCollection.findOne(query);
             res.send(result);
         })
 
