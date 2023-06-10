6a1cf46e992330016c93cb93c1340206158a09ec
zubayer22 <zubayerkh@gmail.com>
2023-06-10 16:03:51 +0600

    jwt added

--- index.js
+++ index.js
@@ -85,6 +85,20 @@ async function run() {
             res.send(result);
         })
 
+        //check admin
+        app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email;
+
+            if (req.decoded.email !== email) {
+                res.send({ admin: false })
+            }
+
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            const result = { admin: user?.role === 'admin' };
+            res.send(result);
+        })
+
         app.patch('/users/admin/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: new ObjectId(id) };
@@ -133,11 +147,17 @@ async function run() {
 
         //cart collection
 
-        app.get('/carts', async (req, res) => {
+        app.get('/carts', verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([])
             }
+
+            const decodedEmail = req.decoded.email;
+            if (email !== decodedEmail) {
+                return res.status(403).send({ error: true, message: 'forbidden access' })
+            }
+
             const query = { email: email }
             const result = await cartCollection.find(query).toArray();
             res.send(result);
